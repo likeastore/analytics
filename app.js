3104dea6b48f915e10941576b1091c6310d23025
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-10 12:01:19 +0200

    dependencies

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var seismo = require('seismo');
 var config = require('./config');
+var logger = require('./source/utils/logger');
 
 seismo.start(config, function () {
-
+	var env = process.env.NODE_ENV || 'development';
+	logger.info('Likeastore analytics listening on port ' + seismo.get('port') + ' ' + env + ' mongo: ' + config.connection);
 });
