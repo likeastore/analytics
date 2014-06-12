029b769b98d4eefed6b829c980ed7689f5cfd8da
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-06-12 18:14:10 +0300

    added jonas account

--- config/production.config.js
+++ config/production.config.js
@@ -12,6 +12,7 @@ var config = {
 	users: {
 		'alexanderbeletsky': '$2a$12$8OWQYmioFTci9/kGppVujeEmFkOVCumMRVkYKg42fopiE9nYuD4uW',
 		'voronianski': '$2a$12$5JkTzjpF8HcJNUrHrT.OiOqs80lXcaABNUKRTY51N.EJh240vsuJe',
+		'jonasbarenfeld': '$2a$12$Bo1aUwhWMvTcGjds5hr.kO6AeVBWYJuRvhEe7YwPzaQAzyklL5CPu',
 		'likeastore': '$2a$12$gS6K/5dt8ECKnB93BoVnse7qXTeljAH7dSlLZZrO7TPdLEYZwjnrG'
 	}
 };
